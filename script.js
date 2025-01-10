55521e3ef674f834dacff3f6f4e90d500438dae3
Denny Rubino <rubidenny@gmail.com>
2025-01-10 15:05:58 +0100

    Fix html ids

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-//iD Element
-const imgCard = document.getElementById('img-gallery');
-
-const title = document.getElementById("title");
+// iD Element
+const imgCardElm = document.getElementById('img-gallery');
+const titleElm = document.getElementById("title");
 
 // Funzione per generare immagini e titolo
 const imgGenerator = () => {
@@ -9,30 +8,28 @@ const imgGenerator = () => {
         .get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         .then((res) => {
             
-            imgCard.innerHTML = "";
-            
-            title.innerHTML = "";
+            imgCardElm.innerHTML = "";
+            titleElm.innerHTML = "";
 
             res.data.forEach((img) => {
-                //Funzione per creare la card completa con la responsive  
+                // Funzione per creare la card completa con la responsive  
                 const col = document.createElement('div');
                 col.className = 'col-sm-12 col-md-6 col-lg-4';
                 col.innerHTML = `
                 <div class="card custom-card position-relative">             
                     <img src="${img.url}" class="card-img-top">
                     <div class="card-body custom-card-body">
-                        <p class="card-title text-center">${img.title}</p>
+                        <p class="card-titleElm text-center">${img.title}</p>
                     </div>
                 </div>
                 `;
 
-                imgCard.appendChild(col);
-
-                
+                imgCardElm.appendChild(col);
             });
-        })
-        
+        });
 };
 
-//Generazione delle card
-imgGenerator();
+
+
+// Generazione delle card
+imgGenerator(); 
