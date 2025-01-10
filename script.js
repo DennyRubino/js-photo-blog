dfec2eb487bef935771188b07b45971d5d616db1
Denny Rubino <rubidenny@gmail.com>
2025-01-10 11:52:47 +0100

    Fix title

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 //iD Element
 const imgCard = document.getElementById('img-gallery');
 
-// Funzione per generare immagini
+const title = document.getElementById("title");
+
+// Funzione per generare immagini e titolo
 const imgGenerator = () => {
     axios
         .get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         .then((res) => {
             
             imgCard.innerHTML = "";
+            
+            title.innerHTML = "";
 
             res.data.forEach((img) => {
-                //Funzione per creare la card con la responsive  
+                //Funzione per creare la card completa con la responsive  
                 const col = document.createElement('div');
-                col.className = 'col-sm-12 col-md-6 col-lg-4 ';
+                col.className = 'col-sm-12 col-md-6 col-lg-4';
                 col.innerHTML = `
-                <img src="img/pin.svg" class="move-pin " style="top: 5%; left: 45%;  height:10%" ">
                 <div class="card custom-card position-relative">             
-                            <img src="${img.url}" class="card-img-top">
-                            <div class="card-body custom-card-body">
-                                <p>Lorem ipsum</p>
-                        </div>
+                    <img src="${img.url}" class="card-img-top">
+                    <div class="card-body custom-card-body">
+                        <p class="card-title text-center">${img.title}</p>
                     </div>
+                </div>
                 `;
+
                 imgCard.appendChild(col);
+
+                
             });
         })
         
